452c9f8ed4c99659059141155c4d23365263af04
ZouariOmar <zouariomar20@gmail.com>
2025-10-16 11:32:00 +0100

    feat(book): ➕ Add `Book` entity with CRUD
    [DETAILS]
    src
    ├── Controller
    │ └── A BookController.php
    ├── Entity
    │ └── A Book.php
    ├── Form
    │ └── A BookType.php
    └── Repository
        └── A BookRepository.php
    migrations
    └── A Version20251016092540.php
    templates
    └── A book/*

---
+++ src/Controller/BookController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BookController extends AbstractController
+{
+    #[Route('/book', name: 'app_book')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+
+    #[Route("book/show", name: 'app_book_show')]
+    public function showAllBooks(EntityManagerInterface $emi): Response
+    {
+        return $this->render("book/show.html.twig", [
+          'books' => $emi->getRepository(Book::class)->findAll()
+        ]);
+    }
+
+    #[Route('book/new', name: 'app_book_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $book = $form->getData();
+            $em->persist($book);
+            $em->flush();
+            return $this->redirectToRoute('app_book_show');
+        }
+        return $this->render("book/new.html.twig", ['form' => $form->createView()]);
+    }
+
+    #[Route('book/delete/{id}', 'app_book_delete')]
+    public function delete(EntityManagerInterface $em, int $id): Response
+    {
+        $er = $em->getRepository(Book::class);
+        $entity = $er->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('No entity found for id ' . $id);
+        }
+        $em->remove($entity);
+        $em->flush();
+        return $this->redirectToRoute("app_book_show");
+    }
+
+    #[Route('book/edit/{id}', name: 'app_book_edit')]
+    public function edit(Request $request, EntityManagerInterface $em, Book $id): Response
+    {
+        $er = $em->getRepository(Book::class);
+        $entity = $er->find($id);
+        if (!$entity) {
+            throw $this->createNotFoundException('No entity found for id ' . $id);
+        }
+
+        $form = $this->createForm(BookType::class, $entity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entity = $form->getData();
+            $em->persist($entity);
+            $em->flush();
+            return $this->redirectToRoute('app_book_show');
+        }
+
+        return $this->render("book/edit.html.twig", ['form' => $form->createView()]);
+    }
+}
--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AuthorRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -21,6 +23,18 @@ class Author
     #[Assert\Email]
     #[ORM\Column(length: 255)]
     private ?string $email = null;
+
+    /**
+     * @var Collection<int, Book>
+     */
+    #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'author')]
+    private Collection $books;
+
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -47,6 +61,36 @@ class Author
     public function setEmail(string $email): static
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): static
+    {
+        if (!$this->books->contains($book)) {
+            $this->books->add($book);
+            $book->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): static
+    {
+        if ($this->books->removeElement($book)) {
+            // set the owning side to null (unless already changed)
+            if ($book->getAuthor() === $this) {
+                $book->setAuthor(null);
+            }
+        }
 
         return $this;
     }
